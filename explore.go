d0509fd499d186b1e4a0710b25149828b6c71e37
high5 <high525jp@gmail.com>
2015-09-25 19:19:53 +0900

    Added more documentation

--- explore.go
+++ explore.go
@@ -1,3 +1,5 @@
+// Package explore provides access to qiita`s tags(items and stocks)
+// The data will be collected from qiitas website at http://qiita.com/tags/{tag}/stocks and http://qiita.com/tags/{tag}/items
 package explore
 
 import (
@@ -7,30 +9,45 @@ import (
 	"strings"
 )
 
+// NewExplore is the main entry point of the explore package.
+// It provides access to the API of this package by returning a Article datastructure.
+// Usage:
 // explore := explore.NewExplore()
-// initialize method
+// stocks, err := explore.GetItems("swift", 1)
+// items, err := explore.GetItems("Go", 1)
+// Usage:
+//   explore := explore.NewExplore()
+//   stocks, err := explore.GetItems("swift", 1)
+//   items, err := explore.GetItems("Go", 1)
 func NewExplore() *Explore {
 	t := Explore{}
 	return &t
 }
 
+// GetStocks provides a slice of Article(recently stocked) filtered by the given tag and page.
+// tag can be filtered by applying a tag by your choice. The input must be a known tag by Qiita.
+// page can be filtered by applying by one page of pagers. If an empty int will be applied first page (1) will be the default.
 func (t *Explore) GetStocks(tag string, page ...int) ([]Article, error) {
 	var pageQuery int = 1
   if (len(page) > 0) {
 		pageQuery = page[0]
 	}
-	return t.GetArticles(tag, "stocks", pageQuery)
+	return t.getArticles(tag, "stocks", pageQuery)
 }
 
+// GetItems provides a slice of Article(articles newly posted) filtered by the given tag and page.
+// tag can be filtered by applying a tag by your choice. The input must be a known tag by Qiita.
+// page can be filtered by applying by one page of pagers. If an empty int will be applied first page (1) will be the default.
 func (t *Explore) GetItems(tag string, page ...int) ([]Article, error) {
 	var pageQuery int = 1
   if (len(page) > 0) {
 		pageQuery = page[0]
 	}
-	return t.GetArticles(tag, "items", pageQuery)
+	return t.getArticles(tag, "items", pageQuery)
 }
 
-func (t *Explore) GetArticles(tag string, category string, page ...int) ([]Article, error) {
+
+func (t *Explore) getArticles(tag string, category string, page ...int) ([]Article, error) {
 	var articles []Article
 
 	var pageQuery int = 1
@@ -50,14 +67,12 @@ func (t *Explore) GetArticles(tag string, category string, page ...int) ([]Artic
 	}
 
 	doc.Find("article").Each(func(i int, s *goquery.Selection) {
-		articleTitle := t.trim(s.Find(".publicItem_body a").First().Text())
-	  articlePath, articleExists := s.Find(".publicItem_body a").First().Attr("href")
-		articleURL := t.appendBaseHostToPath(articlePath, articleExists)
+		title := t.trim(s.Find(".publicItem_body a").First().Text())
+	  path, articleExists := s.Find(".publicItem_body a").First().Attr("href")
+		URL := t.appendBaseHostToPath(path, articleExists)
 
-		//println(s.Find(".tagList_item a").First().Text())
-		//hoge := s.Find(".tagList_item a")
+		// Collect tag
 		var tags []string
-
 		s.Find(".tagList_item a").Each(func(i int, s *goquery.Selection) {
 			tags = append(tags, s.Text())
 		})
@@ -72,9 +87,9 @@ func (t *Explore) GetArticles(tag string, category string, page ...int) ([]Artic
 		}
 
 		a := Article{
-			ArticleTitle: articleTitle,
-			ArtiClePath: articlePath,
-			ArticleURL:  articleURL,
+			Title: title,
+			Path: path,
+			URL:  URL,
 			Tags: tags,
 			UserName:    userName,
 			UserPath:    userPath,
--- types.go
+++ types.go
@@ -5,21 +5,45 @@ import (
 	"net/url"
 )
 
-
+// Internal used constants related to qiita`s website / structure.
 const (
 	baseHost       = "http://qiita.com"
 	basePath       = "/tags/"
 )
 
+// Explore reflects the main datastructure of this package.
+// It doesn`t provide an exported state, but based on this the methods are called.
+// To recieve a new instance just add
+//
+//		package main
+//
+//		import (
+//			"github.com/high5/go-qiita-explore"
+//		)
+//
+//		func main() {
+//			explore := trending.NewExplore()
+//			...
+//		}
+//
 type Explore struct {
 	document *goquery.Document
 }
 
 
+// Article provides information as printed on the source website http://qiita.com/tags/{tag}/stocks and http://qiita.com/tags/{tag}/items.
+// Title is the title of the article. "中規模Web開発のためのMVC分割とレイヤアーキテクチャ"
+// Path is the path of the article. "/yuku_t/items/961194a5443b618a4cac"
+// URL is the http address of the article reflected as url.URL datastructure like "http://qiita.com/yuku_t/items/961194a5443b618a4cac".
+// Tags are a collection of tag.
+// UserName is name of the user who posted the article.
+// UserPath is path of the user who posted the article.
+// User is the http address of the user page of the article reflected as url.URL datastructure like "http://qiita.com/yuku_t".
+// StockCount is the number of qiita stock.
 type Article struct {
-	ArticleTitle    string
-	ArtiClePath     string
-	ArticleURL     *url.URL
+	Title    string
+	Path     string
+	URL     *url.URL
 	Tags           []string
 	UserName       string
 	UserPath       string
